138dca5e793be6a96a43813969fb9c541c444a6c
mdcra <maria.ramos21@upr.edu>
2023-10-30 19:25:19 -0400

    ejemplos clase 30 oct

--- maze.cpp
+++ maze.cpp
@@ -17,44 +17,40 @@ bool findCheese(int row, int col)
 {
     // YOUR CODE HERE
 
-    if ( row == 0 ){
-        // abajo: row++
-    }
-
-    if ( col == 0){
-        // derecha : col++
-    }
-
-    if ( row == 5 ){
-        // abajo: row--
-    }
-
-    if ( col == 5){
-        // derecha : col--
-    }
- 
-    if ( row != 0 && row!= 5 && col != 0 && col!= 5 ){
-        // move all directions
-    }
-
-
-    if (maze[row][col] == 0){
-        // pared
-    }
-
-    if (maze[row][col] == 2){
-        // camino sin visitar
-        maze[row][col] = 1;
-    }
-
-    if (maze[row][col] == 1){
-        // camino ya visitado
+    // base case
+    if ( row < 0 || row > 5 || col < 0 || col > 5 ){
+       return false ; 
     }
 
     if (maze[row][col] == 3){
         // encontramos queso
-        return true
+        return true ;
     }
+
+    if (maze[row][col] == 0){
+        // pared
+        return false; 
+    }
+
+    if (maze[row][col] == 1){
+        // camino ya visitado
+        return false ; 
+    }
+
+    // camino sin visitar
+    maze[row][col] = 1;
+
+    // derecha: col++
+    if (findCheese(row, col+1))  return true ; 
+
+     // abajo: row++
+    if (findCheese(row+1, col)) return true;
+
+    // izquierda: col --
+    if(findCheese(row, col-1)) return true;
+
+    // arriba: row--
+    if(findCheese(row-1, col)) return true;
 
 
     return false; //Dummy Return
@@ -62,7 +58,48 @@ bool findCheese(int row, int col)
 
 string findPositionCheese(int row, int col)
 {
-    // YOUR CODE HERE
+    // base case
+    if ( row < 0 || row > 5 || col < 0 || col > 5 ){
+       return "" ; 
+    }
+
+    if (maze[row][col] == 3){
+        // encontramos queso
+        return "["+to_string(row) +","+to_string(col)+"]" ;
+    }
+
+    if (maze[row][col] == 0){
+        // pared
+        return ""; 
+    }
+
+    if (maze[row][col] == 1){
+        // camino ya visitado
+        return "" ; 
+    }
+
+    // camino sin visitar
+    maze[row][col] = 1;
+
+    string result = "" ;
+    
+    // derecha: col++
+    result = findPositionCheese(row, col+1) ;
+    if (result != "")  return "["+to_string(row) +","+to_string(col)+"]" + result; 
+
+     // abajo: row++
+     result = findPositionCheese(row+1, col) ;
+    if (result != "") return "["+to_string(row) +","+to_string(col)+"]" + result;
+
+    // izquierda: col --
+    result = findPositionCheese(row, col-1) ; 
+    if(result != "") return "["+to_string(row) +","+to_string(col)+"]" + result;
+
+    // arriba: row--
+    result = findPositionCheese(row-1, col) ;
+    if(result != "") return "["+to_string(row) +","+to_string(col)+"]" + result;
+
+
     return ""; //Dummy Return
 }
 
@@ -70,7 +107,7 @@ int main()
 {
     cout << boolalpha;
 
-    cout << findCheese(0, 0) << endl;
+    // cout << findCheese(0, 0) << endl;
     cout << findPositionCheese(0, 0) << endl;
 
 }
